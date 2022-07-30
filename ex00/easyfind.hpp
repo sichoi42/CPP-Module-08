190b1f506efd17ef11d174c23794ccf317b7dadb
sichoi42 <42.4.sichoi@gmail.com>
2022-07-31 00:50:07 +0900

    :sparkles: add ex00
    add ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sichoi <sichoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 23:46:28 by sichoi            #+#    #+#             */
+/*   Updated: 2022/07/31 00:46:44 by sichoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+# define GREEN "\033[0;32m"
+# define RED "\033[0;31m"
+# define RESET "\033[0m"
+
+# include <algorithm>
+# include <iterator>
+# include <stdexcept>
+# include <iostream>
+# include <vector>
+# include <deque>
+# include <list>
+
+template <typename T>
+typename T::iterator easyfind(T& type, int target)
+{
+	typename T::iterator	iter = std::find(std::begin(type), std::end(type), target);
+	if (iter == std::end(type))
+		throw (std::runtime_error(std::string("cannot found!!")));
+	return (iter);
+}
+
+template <typename T>
+void	test(T& type, int target)
+{
+	try
+	{
+		typename T::iterator iter = easyfind(type, target);
+		std::cout << "value: " << *iter << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sichoi <sichoi@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/30 23:09:17 by sichoi            #+#    #+#             */
+/*   Updated: 2022/07/31 00:49:43 by sichoi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int	main(void)
+{
+	std::cout << GREEN << "test vector" << RESET << std::endl;
+
+	std::vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	test(v, 3);
+	test(v, 6);
+
+	std::cout << GREEN << "test deque" << RESET << std::endl;
+
+	std::deque<int> dq;
+	dq.push_front(1);
+	dq.push_back(2);
+	dq.push_front(3);
+	dq.push_back(4);
+	dq.push_front(5);
+	dq.push_back(6);
+	test(dq, 3);
+	test(dq, 0);
+
+	std::cout << GREEN << "test list" << RESET << std::endl;
+
+	std::list<int> lst;
+
+	lst.push_front(1);
+	lst.push_back(2);
+	lst.push_front(3);
+	lst.push_back(4);
+	lst.push_front(5);
+	lst.push_back(6);
+	test(dq, 3);
+	test(dq, 0);
+	return (0);
+}
